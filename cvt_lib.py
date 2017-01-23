916eea290f9405537bdbe8bf961ab10af32ccfbe
Lukas Bystricky <lb13f@my.fsu.edu>
2017-01-23 15:31:33 -0500

    added function to compute energy and do a full CVT

--- cvt_lib.py
+++ cvt_lib.py
@@ -19,7 +19,6 @@ def histogram(imdata):
 	plt.xlabel("Shade")
 	plt.ylabel("Number of pixels")
 	
-	plt.show()
 	return 0
 
 def cvt_render(imdata, generators):
@@ -75,3 +74,46 @@ def cvt_step(imdata, generators_old):
 	generators_new = map(truediv, bins, bin_count)
 					
 	return generators_new
+
+def cvt(imdata, generators, tol, max_iter):
+	
+	#compute initial energy
+	E = []
+	E.append(compute_energy(imdata, generators))
+	
+	dE = float("inf")
+	it = 0
+	
+	while (dE > tol and it < max_iter):
+		generators = cvt_step(imdata, generators)
+		
+		
+		E.append(compute_energy(imdata, generators))
+		it += 1
+		dE = abs(E[it] - E[it-1])/E[it]
+	
+	return (generators, E, it)
+	
+def compute_energy(imdata, generators):
+	
+	shape = imdata.shape #shape[0] = # rows, shape[1] = # columns
+	energy =  0
+	
+	#loop over all pixels
+	for i in range(0, shape[0]):
+		for j in range(0, shape[1]):
+			
+			min_dist = float("inf")
+			k_opt = 0
+			
+			# loop over generators
+			for k in range(len(generators)):
+				dist = abs(imdata[i,j] - generators[k])
+				
+				if dist < min_dist:
+					min_dist = dist
+					k_opt = k
+					
+			energy += min_dist
+					
+	return energy
