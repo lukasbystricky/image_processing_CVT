90848ff6ed9e48d030c095d8110f1f90a0c4014c
Lukas Bystricky <lb13f@my.fsu.edu>
2017-01-23 13:05:03 -0500

    added histrogram function

--- cvt_lib.py
+++ cvt_lib.py
@@ -1,7 +1,24 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+import numpy as np
+
 def read_image(imname):
-	from scipy import misc
-	
+
 	# load image
 	data = misc.imread(imname) 
 	
 	return data
+
+def histogram(imdata):
+
+	im1d = np.reshape(imdata, (np.product(imdata.shape)),1)
+	plt.hist(im1d)
+	
+	plt.title("Histogram of Shades")
+	plt.xlabel("Shade")
+	plt.ylabel("Number of pixels")
+	
+	plt.show()
+	return 0
+
+
