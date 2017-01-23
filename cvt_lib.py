24553426a42b02e95001073efa13ea5e34e41d7a
Lukas Bystricky <lb13f@my.fsu.edu>
2017-01-23 14:05:49 -0500

    added function to render image given set of generators

--- cvt_lib.py
+++ cvt_lib.py
@@ -21,4 +21,27 @@ def histogram(imdata):
 	plt.show()
 	return 0
 
+def cvt_render(imdata, generators):
 
+	shape = imdata.shape #shape[0] = # rows, shape[1] = # columns
+	imdata_new = np.zeros(shape)
+	
+	#loop over all pixels
+	for i in range(0, shape[0]):
+		for j in range(0, shape[1]):
+			
+			min_dist = float("inf")
+			k_opt = 0
+			
+			# loop over generators
+			for k in range(len(generators)):
+				dist = abs(imdata[i,j] - generators[k])
+				
+				if dist < min_dist:
+					min_dist = dist
+					k_opt = k
+					
+			imdata_new[i,j] = generators[k_opt]
+			
+	
+	return imdata_new
