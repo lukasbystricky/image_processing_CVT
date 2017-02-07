6835bbd456abb02f439f6d836a201986cc3b60a6
Jacob Spainhour <Jacob Spainhour>
2017-02-07 14:53:19 -0500

    Added handling of monochrome images

--- cvt3dw_lib.py
+++ cvt3dw_lib.py
@@ -35,7 +35,8 @@ def cvt_render(imdata, generators, weights, numw):
     
     return imdata_new#, imfloor
 
-def cvt_step(imdata, generators_old, weights_old, numw):#, bin_count_old):
+def cvt_step(imdata, generators_old, weights_old, numw, it):#, bin_count_old):
+    #file = open('output.txt', 'a')    
     #print(weights_old)
     shape = imdata.shape #shape[0] = # rows, shape[1] = # columns
     genshape = generators_old.shape
@@ -56,11 +57,12 @@ def cvt_step(imdata, generators_old, weights_old, numw):#, bin_count_old):
             for k in range(len(generators_old)):
                 dist = (weights_old[k]**numw) * \
                             norm(imdata[i,j] - generators_old[k])**2
-                
+                            
+                #file.write(str(it) +' '+ str(k) +' '+ str(dist) + '\n')
                 if dist < min_dist:
                     min_dist = dist
                     k_opt = k
-                    
+            #file.write('\n')        
             #print(k_opt)                    
             bins[k_opt] += imdata[i,j]
             bin_count[k_opt] += 1
@@ -68,22 +70,27 @@ def cvt_step(imdata, generators_old, weights_old, numw):#, bin_count_old):
     #Fixes divide by zero error    
     for elem in range(len(bin_count)):
         if bin_count[elem] == 0:
-            bin_count[elem] = 1
-    
+            bin_count[elem] = 1#imdata.size/3/genshape[0]
+            bins[elem] = np.mean(generators_old, axis = 0)
+            
+    #print(generators_old)
     for i in range(genshape[0]):
         generators_new[i] = bins[i] / bin_count[i] 
         weights_new[i] = bin_count[i]
     #print(weights_new)
+    #print(generators_new)
+    #file.close()
+    #print(weights_new)
     return generators_new, weights_new
 
-def cvt(imdata, generators, tol, max_iter, numw):
+def  cvt(imdata, generators, tol, max_iter, numw):
     
     #compute initial energy
     it = 0
     genshape = generators.shape   
     weights = np.zeros(genshape[0]) + imdata.size/3/genshape[0]
     E = []
-    E.append(compute_energy(imdata, generators, weights, numw))
+    #E.append(compute_energy(imdata, generators, weights, numw))
      
     
     dE = float("inf")
@@ -91,12 +98,15 @@ def cvt(imdata, generators, tol, max_iter, numw):
     
     while (dE > tol and it < max_iter):
         print("Iteration " + str(it + 1)) 
+        misc.imsave("itfolder/iteration" + str(it) + ".png", \
+                cvt_render(imdata, generators, weights, numw))
         
-        generators, weights = cvt_step(imdata, generators, weights, numw)
+        generators, weights = cvt_step(imdata, generators, weights, numw, it)
         
-        E.append(compute_energy(imdata, generators, weights, numw))
+        
+        #E.append(compute_energy(imdata, generators, weights, numw))
         it += 1
-        dE = abs(E[it] - E[it-1])/E[it]
+        #dE = abs(E[it] - E[it-1])/E[it]
     
     return generators, E, it, weights
     
@@ -123,3 +133,4 @@ def compute_energy(imdata, generators, weights, numw):
             energy += min_dist
                     
     return energy
+
